cf792cd43b43cf537148e435db62429b90a99789
Fuadmikayil <Fuadmikayil>
2024-09-01 11:39:39 +0400

    Object add

---
+++ app.js
@@ -0,0 +1,19 @@
+const laptop = {
+    vendor : "Apple",
+    model: "Macbook Pro 16",
+    price : 4999,
+    ram:{
+        type : "Unified",
+        capacity : 18,
+        capactySize: "GB",
+        speed:3200,
+        speedSize: "Mhz"
+    },
+    stoarage : 512,
+    Cpu: {
+        name:"M3",
+        type:"Selicon",
+        speed:"5.3Ghz",
+        core:12
+    }
+}
