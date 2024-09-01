64577ac9de9c0de4b6670541617a9ee229e94e7b
Fuadmikayil <Fuadmikayil>
2024-09-01 13:14:56 +0400

    add2 function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-function add(a,b,c=100) {
-    return a+b+c
+function add(a, b, c = 100) {
+    return a + b + c
 }
-let x=5,y=10,z;
-let result = add(x,y,z)
+const add2 = (a, b, c = 100) => a + b + c
+
+let x = 5, y = 10, z;
+let result = add(x, y, z)
 console.log(result);
